8baa601d36e65fffb9334e158227a55cb2141b8c
Arumi <arumi@thwiki.cc>
2021-12-23 00:59:50 +0800

    Disabled Chinese variant convertion

--- includes/language/converters/ZhConverter.php
+++ includes/language/converters/ZhConverter.php
@@ -34,31 +34,13 @@ class ZhConverter extends LanguageConverter {
 
 		$variants = [
 			'zh',
-			'zh-hans',
-			'zh-hant',
-			'zh-cn',
-			'zh-hk',
-			'zh-mo',
-			'zh-my',
-			'zh-sg',
-			'zh-tw'
 		];
 
 		$variantfallbacks = [
 			'zh' => [ 'zh-hans', 'zh-hant', 'zh-cn', 'zh-tw', 'zh-hk', 'zh-sg', 'zh-mo', 'zh-my' ],
-			'zh-hans' => [ 'zh-cn', 'zh-sg', 'zh-my' ],
-			'zh-hant' => [ 'zh-tw', 'zh-hk', 'zh-mo' ],
-			'zh-cn' => [ 'zh-hans', 'zh-sg', 'zh-my' ],
-			'zh-sg' => [ 'zh-hans', 'zh-cn', 'zh-my' ],
-			'zh-my' => [ 'zh-hans', 'zh-sg', 'zh-cn' ],
-			'zh-tw' => [ 'zh-hant', 'zh-hk', 'zh-mo' ],
-			'zh-hk' => [ 'zh-hant', 'zh-mo', 'zh-tw' ],
-			'zh-mo' => [ 'zh-hant', 'zh-hk', 'zh-tw' ],
 		];
 		$ml = [
 			'zh' => 'disable',
-			'zh-hans' => 'unidirectional',
-			'zh-hant' => 'unidirectional',
 		];
 
 		parent::__construct( $langobj, 'zh',
@@ -68,58 +50,13 @@ class ZhConverter extends LanguageConverter {
 			$ml );
 		$names = [
 			'zh' => '原文',
-			'zh-hans' => '简体',
-			'zh-hant' => '繁體',
-			'zh-cn' => '大陆',
-			'zh-tw' => '台灣',
-			'zh-hk' => '香港',
-			'zh-mo' => '澳門',
-			'zh-sg' => '新加坡',
-			'zh-my' => '大马',
 		];
 		$this->mVariantNames = array_merge( $this->mVariantNames, $names );
 	}
 
 	protected function loadDefaultTables() {
 		$this->mTables = [
-			'zh-hans' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2Hans ),
-			'zh-hant' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2Hant ),
-			'zh-cn' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2CN ),
-			'zh-hk' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2HK ),
-			'zh-mo' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2HK ),
-			'zh-my' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2CN ),
-			'zh-sg' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2CN ),
-			'zh-tw' => new ReplacementArray( MediaWiki\Languages\Data\ZhConversion::$zh2TW ),
 			'zh' => new ReplacementArray
 		];
-	}
-
-	protected function postLoadTables() {
-		$this->mTables['zh-cn']->setArray(
-			$this->mTables['zh-cn']->getArray() + $this->mTables['zh-hans']->getArray()
-		);
-		$this->mTables['zh-hk']->setArray(
-			$this->mTables['zh-hk']->getArray() + $this->mTables['zh-hant']->getArray()
-		);
-		$this->mTables['zh-mo']->setArray(
-			$this->mTables['zh-mo']->getArray() + $this->mTables['zh-hant']->getArray()
-		);
-		$this->mTables['zh-my']->setArray(
-			$this->mTables['zh-my']->getArray() + $this->mTables['zh-hans']->getArray()
-		);
-		$this->mTables['zh-sg']->setArray(
-			$this->mTables['zh-sg']->getArray() + $this->mTables['zh-hans']->getArray()
-		);
-		$this->mTables['zh-tw']->setArray(
-			$this->mTables['zh-tw']->getArray() + $this->mTables['zh-hant']->getArray()
-		);
-	}
-
-	/**
-	 * @param string $key
-	 * @return string
-	 */
-	public function convertCategoryKey( $key ) {
-		return $this->autoConvert( $key, 'zh' );
 	}
 }
